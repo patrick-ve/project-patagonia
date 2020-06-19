c9e782295d6d6fcc4fe76f6ff09dd20b3ecdc143
Patrick van Everdingen <patrick@skybox.org>
2020-06-19 16:23:13 +0200

    :art: Add new recipients of mail, move nodemailer to utils folder
    Iris de Heer has now been added as a recipient, nodemailer module has been moved to a different folder

--- server.js
+++ server.js
@@ -7,42 +7,13 @@ if (process.env.NODE_ENV !== 'production') {
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const consola = require('consola')
-const nodemailer = require('nodemailer')
 const cron = require('node-cron');
+const sendMail = require('./utils/sendMail.js')
 
 // Required selectors
 const website = {
   url: 'http://www.parquetorresdelpaine.cl/es/sistema-de-reserva-de-campamentos-1',
   DOMelement: 'div.content'
-}
-
-async function sendMail() {
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: process.env.MAIL_SERVER,
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.MAIL_ACCOUNT,
-        pass: process.env.MAIL_PASSWORD
-    }
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Skere Skraper Engine 🚂" <patrick@skere-skraper.app>', // sender address
-    to: ["vincent.de.wolf@hotmail.com", "patrick@skybox.org"], // list of receivers
-    subject: "Dit is een testbericht van de skere skraper engine 🧪", // Subject line
-    text: "Ga gauw naar http://www.parquetorresdelpaine.cl/es/sistema-de-reserva-de-campamentos-1 toe en BOEK DAN!!!! ", // plain text body
-    html: "<h1>Ga gauw naar <a href='http://www.parquetorresdelpaine.cl/es/sistema-de-reserva-de-campamentos-1' target='_blank'>de website</a> toe en BOEK DAN!!!!</h1>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 // Run cron job every hour
